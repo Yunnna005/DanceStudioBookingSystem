8adf00aa9ef4bb323ec14c1b68aac2137c2a88aa
STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>
2024-01-15 13:03:00 +0000

    Signed-off-by: STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>

---
+++ DanceStudioBookingSystem/DBConnect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace DanceStudioBookingSystem
+{
+    internal class DBConnect
+    {
+        //public const String oraDB = "Data Source = localhost/orcl; User ID = AK; Password = 123456;";
+        public const String oraDB = "Data Source = localhost/orcl; User ID = AK; Password = ;";
+    }
+}
---
+++ DanceStudioBookingSystem/frmDBConnect.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace DanceStudioBookingSystem
+{
+    partial class frmDBConnect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(252, 159);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(249, 60);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect/Disconnect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblState.Location = new System.Drawing.Point(323, 253);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(99, 32);
+            this.lblState.TabIndex = 1;
+            this.lblState.Text = "CLOSED";
+            // 
+            // frmDBConnect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "frmDBConnect";
+            this.Text = "frmDBConnect";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblState;
+    }
+}
